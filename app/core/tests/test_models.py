c43522ce62f48ba1d6aae764349e30eb09681450
ByunHuiYong <expectbhyy@gmail.com>
2020-05-17 19:12:29 +0900

    modified django user model

---
+++ app/core/tests/test_models.py
@@ -0,0 +1,45 @@
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+
+
+class ModelTests(TestCase):
+
+    def test_create_user_with_email_successful(self):
+        """Test creating a new user with an email is successful"""
+        email = 'expectbhyy@gmail.com'
+        password = 'Testpass123'
+        user = get_user_model().objects.create_user(
+            email=email,
+            password=password
+        )
+
+        self.assertEqual(user.email, email)
+        self.assertTrue(user.check_password(password))
+
+    def test_new_user_email_normalized(self):
+        """Test_the_email_for_a_new_user_is_normalized"""
+        email = 'test@gmail.com'
+        user = get_user_model().objects.create_user(email, 'test123')
+
+        self.assertEqual(user.email, email.lower())
+
+    def test_new_user_invalid_email(self):
+        """Test creating user with no email raises error"""
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user(None, 'test123')
+
+    def test_create_new_superuser(self):
+        """Test creating a new superuser"""
+        user = get_user_model().objects.create_user(
+            'test@gmail.com',
+            'test123',
+        )
+
+        # TODO: Error Point : Traceback (most recent call last):
+        #   File "/app/core/tests/test_models.py", line 38, in test_create_new_superuser
+        #     self.assertTrue(user.is_superuser)
+        #       AssertionError: False is not true
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
+
+
